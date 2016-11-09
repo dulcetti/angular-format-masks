7c6021ff9da67511d0a86c413363915f47621e03
Bruno Dulcetti <brunodulcetti@gmail.com>
2016-11-09 12:55:05 -0200

    Format masks spec Initial tests of component

---
+++ tests/format-masks.spec.js
@@ -0,0 +1,16 @@
+describe('Format Masks', function() {
+    var component;
+    var $compile;
+    var $controller;
+    var $rootScope;
+
+    beforeEach(module('format.masks'));
+    
+    beforeEach(inject(function(_$controller_, _$compile_, _$rootScope_) {
+        $compile = _$compile_;
+        $controller = _$controller_;
+        $rootScope = _$rootScope_;
+    }));
+    
+});
+    
