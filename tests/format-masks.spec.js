97e79dcfaec48683b7939250432dde1bf47a9453
Bruno Dulcetti <brunodulcetti@gmail.com>
2016-11-09 13:31:28 -0200

    Tests Controller Add variables of mask type and mask value Verify if controller component is defined

--- tests/format-masks.spec.js
+++ tests/format-masks.spec.js
@@ -1,20 +1,22 @@
-describe('Format Masks', function() {
+describe('Format Masks Component', function() {
     beforeEach(module('format.masks'));
 
     var component;
     var controller;
+    var type;
+    var value;
     var scope;
 
     beforeEach(inject(function($compile, $rootScope) {
         scope = $rootScope.$new();
-        component = angular.element('<format-masks></format-masks>');
+        component = angular.element('<format-masks mask-value="02545191728" mask-type="CPF"></format-masks>');
         component = $compile(component)(scope);
-        controller = component.controller('formatMasksController');
+        controller = component.controller('formatMasks');
+        type = controller.maskType;
+        value = controller.maskValue;
     }));
     
-    it('Component init', function() {
-        expect(component).toBeDefined();
+    it('Controller of component should to be init', function() {
+        expect(controller).toBeDefined();
     });
-    
 });
-    
