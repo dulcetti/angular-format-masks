2347102258bef2885f31fe7ab80ed922c45130fd
Bruno Dulcetti <brunodulcetti@gmail.com>
2016-11-10 12:09:25 -0200

    Tests of type and value Validate mask types

--- tests/format-masks.spec.js
+++ tests/format-masks.spec.js
@@ -9,32 +9,46 @@ describe('Format Masks Component', function() {
 
     beforeEach(inject(function($compile, $rootScope) {
         scope = $rootScope.$new();
-        component = angular.element('<format-masks mask-value="02545191728" mask-type="CPF"></format-masks>');
+        component = angular.element('<format-masks mask-value="11111111111" mask-type="cpf"></format-masks>');
         component = $compile(component)(scope);
         controller = component.controller('formatMasks');
         type = controller.maskType;
         value = controller.maskValue;
     }));
-    
+
     it('Controller of component should to be init', function() {
         expect(controller).toBeDefined();
     });
     
-    it('Mask value should to be defined', function() {
-        expect(value).toBeDefined();
+    describe('Mask value', function() {
+        it('Mask value should to be defined', function() {
+            expect(value).toBeDefined();
+        });
+        
+        it('Mask value should not to be null', function() {
+            expect(value).not.toBeNull();
+            expect(value).not.toEqual('');
+        });
     });
     
-    it('Mask value should not to be null', function() {
-        expect(value).not.toBeNull();
-        expect(value).not.toEqual('');
+    describe('Mask type', function() {
+        it('Mask type and value should to be defined', function() {
+            expect(type).toBeDefined();
+            expect(value).toBeDefined();
+        });
+        
+        it('Mask type and value should not to be null', function() {
+            expect(type).not.toBeNull();
+            expect(type).not.toEqual('');
+
+            expect(value).not.toBeNull();
+            expect(value).not.toEqual('');
+        });
+
+        it('Mask type as valid', function() {
+            expect(controller.types).toContain(type);
+        });
     });
+
     
-    it('Mask type should to be defined', function() {
-        expect(type).toBeDefined();
-    });
-    
-    it('Mask type should be have a type', function() {
-        expect(type).not.toBeNull();
-        expect(type).not.toEqual('');
-    });
 });
